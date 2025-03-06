d58adf0251b08965caef1f4f061c649c0aa9d15b
Maria Laura Willemann <147657624+marialaura180308@users.noreply.github.com>
2025-03-06 13:43:12 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
     botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-        
+    }
+
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -22,5 +27,3 @@ document.addEventListener('DOMContentLoaded', function(){
     });
     
        })
-           
-});
